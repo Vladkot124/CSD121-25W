74eb314dffc09760bc1070f174e57a9ed68d0f09
Vladkot124 <24017497@saultcollege.ca>
2025-02-09 20:19:39 -0800

    Done assigment

--- src/main/java/lab3/game/Board.java
+++ src/main/java/lab3/game/Board.java
@@ -2,4 +2,67 @@ package lab3.game;
 
 public class Board {
     // TODO: encapsulate the representation of the tictactoe board and provide instance methods to access and update it
+    private char[][] board;
+    private final int SIZE = 3;
+
+    public Board() {
+        board = new char[SIZE][SIZE];
+        initializeBoard();
+    }
+
+    private void initializeBoard() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                board[i][j] = ' ';
+            }
+        }
+    }
+
+    public boolean placeMark(int row, int col, char player) {
+        if (row >= 0 && row < SIZE && col >= 0 && col < SIZE && board[row][col] == ' ') {
+            board[row][col] = player;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkWin() {
+        // Check rows and columns
+        for (int i = 0; i < SIZE; i++) {
+            if ((board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) ||
+                    (board[0][i] != ' ' && board[0][i] == board[1][i] && board[1][i] == board[2][i])) {
+                return true;
+            }
+        }
+
+        // Check diagonals
+        if ((board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) ||
+                (board[0][2] != ' ' && board[0][2] == board[1][1] && board[1][1] == board[2][0])) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean isFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (board[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void printBoard() {
+        System.out.println("-------------");
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println("\n-------------");
+        }
+    }
 }
