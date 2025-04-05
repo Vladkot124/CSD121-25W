f2ab2da82cf8766deb3d07691c1d758dba6e1bc6
Vladkot124 <24017497@saultcollege.ca>
2025-04-05 18:46:59 -0400

    Finally done

--- src/main/java/lab6/Main.java
+++ src/main/java/lab6/Main.java
@@ -1,4 +1,22 @@
 package lab6;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("JavaFX is working!");
+        Scene scene = new Scene(label, 300, 200);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Lab 6 Test");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
